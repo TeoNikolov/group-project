a3348dc811edfd5fee719c78bbae65fe0da456fe
TeoNikolov <tnikolov@hotmail.com>
2017-03-01 20:05:22 +0000

    Improved Properties Exception Handling

--- src/compass/Main.java
+++ src/compass/Main.java
@@ -1,9 +1,23 @@
 package compass;
 
+import compass.exceptions.PropertiesNotLoadedException;
 import compass.utilities.APIInterface;
 
 public class Main {
     public static void main(String[] args) {
-        APIInterface.getWeather(null); // <- This method will get the weather from Dark Sky weather API
+        // Try setting up properties
+        try {
+            CompassProperties tempprops = new CompassProperties();
+            if (tempprops.hasCompleted())
+                Constants.props = tempprops;
+                Constants.propertiesLoaded = true;
+        }  catch (PropertiesNotLoadedException e) {
+            e.printStackTrace();
+        }
+
+        if (Constants.propertiesLoaded) {
+            // Properties have successfully been loaded
+            APIInterface.getWeather(null); // <- This method will get the weather from Dark Sky weather API
+        }
     }
 }
