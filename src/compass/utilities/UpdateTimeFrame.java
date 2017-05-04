7126f766599a5cb9ae4947dc9cf2ac383360772f
Danielh112 <Danielh176@hotmail.co.uk>
2017-05-04 15:42:12 +0100

    Create UpdateTimeFrame.java

---
+++ src/compass/utilities/UpdateTimeFrame.java
@@ -0,0 +1,28 @@
+package compass.utilities;
+
+import compass.Constants;
+
+public class UpdateTimeFrame {
+
+	public static boolean updateTimeFrame(String catagory, Long lastUpdated) {
+
+		Long currentTime = System.currentTimeMillis();
+
+		switch (catagory.toLowerCase()) {
+		case "weather":
+			if ((lastUpdated - currentTime) > Constants.weatherUpdateTime)
+				return true;
+		case "transport":
+			if ((lastUpdated - currentTime) > Constants.transportUpdateTime)
+				return true;
+		case "events":
+			if ((lastUpdated - currentTime) > Constants.eventsUpdateTime)
+				return true;
+		case "general":
+			if ((lastUpdated - currentTime) > Constants.generalUpdateTime)
+				return true;
+		default:
+			return false;
+		}
+	}
+}
